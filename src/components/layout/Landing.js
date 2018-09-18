c2d02e4e0bc948c941967bdc7e9a5d22b83e5f6a
Ricardo Andres Angel Villadiego <ra.angel10@uniandes.edu.co>
2018-09-18 00:52:31 -0500

    Añadidas graficas

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import PieGraph from '../graphs/PieGraph';
-import GraphEdad from '../graphs/graphEdad';
+import GraphEdad from '../graphs/GraphEdad';
+import GraphEducacion from '../graphs/GraphEducacion';
+import GraphEnfermedades from '../graphs/GraphEnfermedades';
+import GraphProfesion from '../graphs/GraphProfesion';
+import GraphQueHace from '../graphs/GraphQueHace';
+import GraphPais from '../graphs/GraphPais';
+import GraphCiudad from '../graphs/GraphCiudad';
+import GraphFechaIngreso from '../graphs/GraphFechaIngreso';
+import GraphIngresos from '../graphs/GraphIngresos';
 
 class Landing extends Component {
   render() {
@@ -8,6 +16,14 @@ class Landing extends Component {
       <div>
         <div className="container-fluid">
           <GraphEdad />
+          <GraphEducacion />
+          <GraphEnfermedades />
+          <GraphProfesion />
+          <GraphQueHace />
+          <GraphPais />
+          <GraphCiudad />
+          <GraphFechaIngreso />
+          <GraphIngresos />
         </div>
       </div>
     );
